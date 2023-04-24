4a78f3df83041c73cc665c7528acc638cc61c78e
Alexbarile <alexbarile93fg@yahoo.it>
2023-04-24 18:40:50 +0200

    create navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+function Navbar(params) {
+    return (
+        <nav className="navbar bg-dark">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 ms-3 h1 text-white">Sushi Xian</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
