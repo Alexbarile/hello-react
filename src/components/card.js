89c62d62d565bc20f561e99c30493911e149a57a
Alexbarile <alexbarile93fg@yahoo.it>
2023-05-03 17:37:06 +0200

    add increment and decrement object card

--- src/components/card.js
+++ src/components/card.js
@@ -5,6 +5,20 @@ class Card extends Component {
         return (
             <div className="col">
                 <div className="card" style={{ width: '18rem', textAlign: 'center' }}>
+                    <div className="row p-0 m-0">
+                        <div className="col-6">
+                            <button onClick={() => this.props.onIncrement(this.props.card)} className="btn btn-primary">
+                                Aggiungi
+                            </button>
+                        </div>
+                        <div className="col-6">
+                            <button onClick={() => this.props.onDecrement(this.props.card)} className="btn btn-primary">
+                                Togli
+                            </button>
+                        </div>
+                    </div>
+                    <hr className='m-0'></hr>
+                    <div className='p-2'><span class="badge bg-warning">{this.props.card.quantità}</span></div>
                     <img src={this.props.card.immagine} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{this.props.card.nome} Roll</h5>
